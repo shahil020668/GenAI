1af826ab730c5de17dd5739945ebeabab17e63c3
zynox <mohammadshahil0068@gmail.com>
2025-12-25 20:43:38 +0530

    msg

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,4 +1,4 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
